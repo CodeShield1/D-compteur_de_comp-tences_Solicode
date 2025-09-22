61de2b5a8c38c67bb67a42fd96552cb924fd3da7
brahim_medkour <medkourbrahim08@.com>
2025-09-22 10:53:36 +0100

    ona fait un boucle  qui boucler touts array

--- prototype/script.js
+++ prototype/script.js
@@ -1,4 +1,13 @@
 function afficherMessage() {
 let codes = ["C1", "C3", "C2", "C1", "C8", "C2", "C1"];
+let codeChoiser =prompt("Entrez le code à chercher (C1, C2, C8) :") ;
+
+let compteur = 0;
+
+for (let i = 0   ;  i < codes.length   ; i++) {
+  if (codes[i] === codeChoiser) {
+    compteur++;
+  }
+}
 }
 afficherMessage();
